c86ff2cdcc7d5a2cc10df263c99c4e283935960f
AYUSH SINGH <ayushsinghpanchet07@gmail.com>
2026-09-22 20:23:55 +0530

    model change

--- apps/1_qna_bot.py
+++ apps/1_qna_bot.py
@@ -5,7 +5,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 import streamlit as st
 
 llm = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash",
+    model="gemini-3.7-flash",
     temperature=1.0,
     max_retries=0,
 )
